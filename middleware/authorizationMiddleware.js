b8f8bebed924d0fcf45bb7d75b5536e3cb8933e1
MManimekalai <manimekalai.eee123@gmail.com>
2023-10-27 03:12:46 +0530

    authentication check

--- middleware/authorizationMiddleware.js
+++ middleware/authorizationMiddleware.js
@@ -1,15 +1,22 @@
-// Middleware to check user roles and permissions
+const { isAuthenticated, hasRole } = require('../utils/authenticationUtils'); // Import your utility functions
+
 module.exports = (role) => {
   return (req, res, next) => {
-    if (!req.user || !req.user.role) {
+    console.log('Inside authentication middleware');
+    
+    // Check if the user is authenticated
+    if (!isAuthenticated(req)) {
+      console.log('User is not authenticated');
       return res.status(401).json({ message: 'Authentication required' });
     }
 
-    if (req.user.role !== role) {
+    // Check if the user has the required role
+    if (!hasRole(req, role)) {
+      console.log('User role does not match expected role');
       return res.status(403).json({ message: 'Forbidden' });
     }
 
-    // If the user's role matches the expected role, proceed to the next middleware or route handler
+    // If the user is authenticated and has the required role, proceed to the next middleware or route handler
     next();
   };
 };
