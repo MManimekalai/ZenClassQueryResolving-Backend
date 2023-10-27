fbec26908737ee47c707f40114aca9a6284e4adb
MManimekalai <manimekalai.eee123@gmail.com>
2023-10-27 22:50:17 +0530

    token

--- middleware/authenticationMiddleware.js
+++ middleware/authenticationMiddleware.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // Middleware to verify the token
 module.exports = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split('  ')[1];
+  const token = authHeader && authHeader.split(' ')[1];
   //console.log(authHeader)
   //console.log(token)
 
