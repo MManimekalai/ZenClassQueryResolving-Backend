b7906833ea1ac990c1a7d617da46f512186d4444
MManimekalai <manimekalai.eee123@gmail.com>
2023-10-29 02:23:37 +0530

    authorization added

--- controllers/mentorController.js
+++ controllers/mentorController.js
@@ -68,8 +68,8 @@ exports.viewTickets = async (req, res) => {
 exports.resolveTicket = async (req, res) => {
   try {
     await connectDB();
-    const  ticketNumber  = req.params;
-    const  solution  = req.body;
+    const  ticketNumber  = req.params.ticketNumber;
+    const  solution  = req.body.solution;
 
     // Find the ticket by its ID
     const ticket = await Ticket.findOne(
